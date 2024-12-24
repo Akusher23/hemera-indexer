017df4caf5c636e36950adb1bb85a2d81ef5d84c
river <river.zeng@thehemera.com>
2024-12-24 22:41:10 +0800

    adjust uniswapv3_with_farming_detail.sql

--- indexer/aggr_jobs/order_jobs/py_jobs/sqls/uniswapv3_with_farming_detail.sql
+++ indexer/aggr_jobs/order_jobs/py_jobs/sqls/uniswapv3_with_farming_detail.sql
@@ -9,7 +9,7 @@ with farming_config as (select decode('827922686190790b37229fd06084350e74485b72'
                                       from erc721_token_transfers d1
                                                inner join farming_config d2
                                                           on d1.token_address = d2.token_address and d1.to_address = d2.to_address
-                                      where block_timestamp < date('{start_date}') + interval  '1 days') t
+                                      where block_timestamp < date('{start_date}') + interval '1 days') t
                                 where rn = 1),
 
 
@@ -28,19 +28,20 @@ with farming_config as (select decode('827922686190790b37229fd06084350e74485b72'
                                  d5.symbol                 AS token0_symbol,
                                  d6.decimals               AS token1_decimals,
                                  d6.symbol                 AS token1_symbol
-                          from af_uniswap_v3_pool_prices_period d1
+                          from (select * from af_uniswap_v3_pool_prices_period where period_date = '{start_date}') d1
                                    inner join af_uniswap_v3_pools d2
                                               on d1.pool_address = d2.pool_address
-                                   inner join af_uniswap_v3_token_data_period d3 on d1.pool_address = d3.pool_address
+                                   inner join (select *
+                                               from af_uniswap_v3_token_data_period
+                                               where d3.period_date = '{start_date}') d3
+                                              on d1.pool_address = d3.pool_address
                                    inner join af_uniswap_v3_tokens d4
                                               ON d3.position_token_address = d4.position_token_address
                                                   AND d3.token_id = d4.token_id
                                    INNER JOIN tokens d5 ON d2.token0_address = d5.address
                                    INNER JOIN tokens d6 ON d2.token1_address = d6.address
-         where d1.period_date = '{start_date}' and d3.period_date = '{start_date}'
-                                    and d5.symbol is not null and d6.symbol is not null
-                                    and (d5.symbol = 'FBTC' or d6.symbol = 'FBTC')
-     ),
+                              and d5.symbol is not null and d6.symbol is not null
+                              and (d5.symbol = 'FBTC' or d6.symbol = 'FBTC')),
 
      farming_detail as (select d1.period_date,
                                d1.nft_address,
