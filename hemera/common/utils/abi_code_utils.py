47cb9ebafdfd5132d7facbe69e4bc87e9d8d40a1
will0x0909 <zhiyuan@thehemera.com>
2025-01-07 13:46:51 +0800

    reformat

--- hemera/common/utils/abi_code_utils.py
+++ hemera/common/utils/abi_code_utils.py
@@ -6,7 +6,6 @@ Author  : xuzh
 Project : hemera_indexer
 """
 import logging
-from itertools import accumulate
 from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
 
 import eth_abi
@@ -34,10 +33,12 @@ from hemera.indexer.utils.abi import (
     abi_bytes_to_bytes,
     abi_string_to_text,
     codec,
+    encode_bool,
     event_log_abi_to_topic,
     function_abi_to_4byte_selector_str,
     get_types_from_abi_type_list,
     pad_address,
+    tuple_encode,
     uint256_to_bytes,
 )
 
@@ -330,93 +331,6 @@ class Function:
         else:
             return encode_data(self._function_abi, arguments, self.get_signature())
         return bytes_to_hex_str(encoded)
-
-
-def zpad(value: bytes, length: int) -> bytes:
-    return value.rjust(length, b"\x00")
-
-
-def zpad_right(value: bytes, length: int) -> bytes:
-    return value.ljust(length, b"\x00")
-
-
-def ceil32(x: int) -> int:
-    return x if x % 32 == 0 else x + 32 - (x % 32)
-
-
-def encode_bool(arg: bool) -> bytes:
-    value = b"\x01" if arg is True else b"\x00"
-    return zpad(value, 32)
-
-
-def encode_uint256(value: int) -> bytes:
-    return value.to_bytes(32, byteorder="big")
-
-
-def encode_address(address: str) -> bytes:
-    address = address.lower().replace("0x", "")
-
-    if len(address) != 40:
-        raise ValueError("Invalid address length")
-
-    padded = "0" * 24 + address
-    return bytes.fromhex(padded)
-
-
-def encode_bytes(value: bytes) -> bytes:
-    value_length = len(value)
-
-    encoded_size = encode_uint256(value_length)
-    padded_value = zpad_right(value, ceil32(value_length))
-
-    return encoded_size + padded_value
-
-
-def tuple_encode(values, type_lis):
-    raw_head_chunks = []
-    tail_chunks = []
-    for value, tp in zip(values, type_lis):
-        if tp == "bytes":
-            raw_head_chunks.append(None)
-            tail_chunks.append(encode_bytes(value))
-        elif tp == "bool":
-            raw_head_chunks.append(encode_bool(value))
-            tail_chunks.append(b"")
-        elif tp == "address":
-            raw_head_chunks.append(encode_address(value))
-            tail_chunks.append(b"")
-        elif tp == "(address,bytes)[]":
-            items_are_dynamic = True
-            if not items_are_dynamic or len(value) == 0:
-                return b"".join(tail_chunks)
-            encoded_size = encode_uint256(len(value))
-
-            tmp_tail_chunks = tuple(tuple_encode(list(i), ["address", "bytes"]) for i in value)
-            head_length = 32 * len(value)
-            tail_offsets = (0,) + tuple(accumulate(map(len, tmp_tail_chunks[:-1])))
-            head_chunks = tuple(encode_uint256(head_length + offset) for offset in tail_offsets)
-            raw_head_chunks.append(None)
-            tail_chunks.append(encoded_size + b"".join(head_chunks + tmp_tail_chunks))
-        elif tp == "(address,bytes)":
-            encoded_size = encode_uint256(len(value))
-            encoded_elements = b""
-            for target, call_data in value:
-                encoded_elements += encode_address(target)
-                encoded_elements += tuple_encode([call_data], ["bytes"])
-            raw_head_chunks.append(None)
-            tail_chunks.append(encoded_size + encoded_elements)
-        else:
-            raise Exception(f"Unsupported type {tp}")
-
-    head_length = sum(32 if item is None else len(item) for item in raw_head_chunks)
-    tail_offsets = (0,) + tuple(accumulate(map(len, tail_chunks[:-1])))
-    head_chunks = tuple(
-        encode_uint256(head_length + offset) if chunk is None else chunk
-        for chunk, offset in zip(raw_head_chunks, tail_offsets)
-    )
-
-    encoded_value = b"".join(head_chunks + tuple(tail_chunks))
-    return encoded_value
 
 
 class FunctionCollection:
