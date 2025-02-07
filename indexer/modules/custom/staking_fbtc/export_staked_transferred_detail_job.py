f71c6893a04e22460acbcd80ec9563f24a08e5fd
river <river.zeng@thehemera.com>
2025-02-07 14:11:00 +0800

    add                                                     '0x3c7c0ebfcd5786ef48df5ed127cddeb806db976c': 'apex'

--- indexer/modules/custom/staking_fbtc/export_staked_transferred_detail_job.py
+++ indexer/modules/custom/staking_fbtc/export_staked_transferred_detail_job.py
@@ -48,7 +48,9 @@ class ExportTransferredFBTCDetailJob(FilterTransactionDataJob):
             self._transferred_protocol_dict = ast.literal_eval(transferred_protocol_dict_str)
             # add woofi、circuit
             self._transferred_protocol_dict.update({'0x82fde5086784e348aed03eb7b19ded97652db7a8': 'woofi',
-                                                    '0x5b27576159d201697feb73e7cbe5dafcfdc9b0dc': 'circuit'})
+                                                    '0x5b27576159d201697feb73e7cbe5dafcfdc9b0dc': 'circuit',
+                                                    '0x3c7c0ebfcd5786ef48df5ed127cddeb806db976c': 'apex'
+                                                    })
 
             self._staked_address_list = config.get(str(chain_id), "STAKED_BTC_ADDRESS").split(
                 ',')  # remember to remove the last one
