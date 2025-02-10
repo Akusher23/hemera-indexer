99f2c863737caffc18155dd4183b10345f280677
will0x0909 <zhiyuan@thehemera.com>
2025-02-10 10:47:02 +0800

    error log

--- hemera/indexer/utils/multicall_hemera/multi_call_helper.py
+++ hemera/indexer/utils/multicall_hemera/multi_call_helper.py
@@ -140,7 +140,7 @@ class MultiCallHelper:
                     call.returns = call.decode_output(result)
                 except Exception:
                     call.returns = None
-                    self.logger.error(f"multicall helper failed call: {call}")
+                    self.logger.error(f"multicall helper failed call: {call}, data {data}")
 
     @calculate_execution_time
     def construct_multicall_rpc(self, to_execute_multi_calls):
