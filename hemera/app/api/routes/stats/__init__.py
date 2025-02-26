0ec37d461feb46b3de6e89314213c859d2bbcfd1
will9709 <zhiyuan@thehemera.com>
2025-02-26 16:23:07 +0800

    add dashboards

--- hemera/app/api/routes/stats/__init__.py
+++ hemera/app/api/routes/stats/__init__.py
@@ -9,10 +9,12 @@ from typing import List
 
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
+from sqlalchemy import desc
 
 from hemera.app.api.deps import ReadSessionDep
 from hemera.app.api.routes.helper.block import get_block_count
 from hemera.app.api.routes.helper.transaction import GasStats, get_gas_stats, get_latest_txn_count, get_total_txn_count
+from hemera.common.models.stats.daily_boards_stats import DailyBoardsStats
 
 router = APIRouter(tags=["STATS"])
 
@@ -54,3 +56,42 @@ async def get_address_profile(session: ReadSessionDep):
         gas_stats=gas_stats,
         top_active_contracts=top_active_contracts,
     )
+
+
+@router.get("/v1/developer/stats/all_boards", response_model=List[str])
+async def get_unique_board_ids(session: ReadSessionDep):
+    query = session.query(DailyBoardsStats.board_id).distinct()
+    result = query.all()
+    unique_board_ids = [row[0] for row in result]
+    return unique_board_ids
+
+
+class ECOBoardResponse(BaseModel):
+    rank: int
+    board_id: str
+    block_date: str
+    key: str
+    count: int
+
+
+@router.get("/v1/developer/stats/get_board_data", response_model=List[ECOBoardResponse])
+async def get_board_data(board_id: str, block_date: str, session: ReadSessionDep):
+    query = (
+        session.query(DailyBoardsStats)
+        .filter(DailyBoardsStats.board_id == board_id, DailyBoardsStats.block_date == block_date)
+        .order_by(desc(DailyBoardsStats.count))
+    )
+
+    result = query.all()
+    data = [
+        ECOBoardResponse(
+            rank=index + 1,
+            board_id=row.board_id,
+            block_date=row.block_date.strftime("%Y-%m-%d"),
+            key=row.key,
+            count=row.count,
+        )
+        for (index, row) in enumerate(result)
+    ]
+
+    return data
