e7cff80a0da930266238e98ffa5ffeede5271596
river <river.zeng@thehemera.com>
2025-08-12 16:36:24 +0800

    improve base sql

--- indexer/aggr_jobs/disorder_jobs/daily_address_token_balances_fbtc.sql
+++ indexer/aggr_jobs/disorder_jobs/daily_address_token_balances_fbtc.sql
@@ -3,17 +3,16 @@ from daily_address_token_balances
 WHERE block_date >= '{start_date}'
   and block_date < '{end_date}';
 
-insert into public.daily_address_token_balances (address, block_date, token_address, token_id, token_type, balance
+insert into public.daily_address_token_balances (address, block_date, token_address, token_type, balance
                                                  )
 
 select address,
        date(block_timestamp) as block_date,
        token_address,
-       null as token_id,
        token_type,
        balance
 from (select *,
-             row_number() over (partition by address,token_address,token_id order by block_timestamp desc) as rn
+             row_number() over (partition by address,token_address order by block_timestamp desc) as rn
       from address_token_balances
       WHERE block_timestamp >= '{start_date}'
         and block_timestamp < '{end_date}'
