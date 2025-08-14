44b7260dd4911d17b8a01c44722f354c7e71597e
river <river.zeng@thehemera.com>
2025-08-14 17:24:34 +0800

    add and balance> 0 for merchantmoe

--- indexer/aggr_jobs/order_jobs/period_feature_holding_balance_merchantmoe.sql
+++ indexer/aggr_jobs/order_jobs/period_feature_holding_balance_merchantmoe.sql
@@ -37,7 +37,7 @@ with moe_pools_table as (select d0.*,
                                               inner join
                                           (select *
                                            from period_address_token_balances
-                                           where token_type = 'ERC1155') d1
+                                           where token_type = 'ERC1155' and balance> 0) d1
                                           on d0.token_address = d1.token_address),
 
      detail_table as (select d1.address
