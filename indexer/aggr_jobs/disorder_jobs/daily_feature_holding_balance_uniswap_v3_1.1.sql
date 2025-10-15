dbed560345c9d6dd35c5d0af1dd43ac037ebe4cb
river <river.zeng@thehemera.com>
2025-10-15 10:07:26 +0800

    add uniswap 1.1 sql

---
+++ indexer/aggr_jobs/disorder_jobs/daily_feature_holding_balance_uniswap_v3_1.1.sql
@@ -0,0 +1,33 @@
+begin;
+delete
+from daily_feature_uniswap_v3_token_details
+where block_date >= '{start_date}'
+  and block_date < '{end_date}';
+insert into daily_feature_uniswap_v3_token_details(nft_address, block_date,token_id, wallet_address, pool_address, liquidity)
+select position_token_address,
+       date(block_timestamp)::DATE as block_date,
+       token_id,
+       wallet_address,
+       COALESCE(pool_address, '')          as pool_address,
+       liquidity
+from (select *, row_number() over (partition by position_token_address, token_id order by block_timestamp desc) rn
+      from af_uniswap_v3_token_data_hist
+      where block_timestamp >= '{start_date}'
+        and block_timestamp < '{end_date}') t
+where rn = 1;
+
+
+delete
+from daily_feature_uniswap_v3_pool_prices
+where block_date >= '{start_date}'
+  and block_date < '{end_date}';
+insert into daily_feature_uniswap_v3_pool_prices(pool_address, block_date, sqrt_price_x96)
+select pool_address,
+       date(block_timestamp)::DATE as block_date,
+       sqrt_price_x96
+from (select *, row_number() over (partition by pool_address order by block_timestamp desc) rn
+      from af_uniswap_v3_pool_prices_hist
+      where block_timestamp >= '{start_date}'
+        and block_timestamp < '{end_date}') t
+where rn = 1;
+commit
